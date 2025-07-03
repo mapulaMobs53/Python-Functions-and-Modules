bbd7b5c8dad02e338b70baf946cf59e232d1b74f
mapulaMobs53 <masekwamengmapula5@gmail.com>
2025-07-03 02:38:34 +0200

    Modules builtins and importing

---
+++ modules_builtin.py
@@ -0,0 +1,15 @@
+import math
+import random
+import datetime
+import os
+import sys
+
+print(math.pi)
+
+print(random.randint(0,10))
+
+print(datetime.datetime.now())
+
+print(os.getcwd()) #prints the location of the file in the pc
+
+print(sys.version)  # system version 
